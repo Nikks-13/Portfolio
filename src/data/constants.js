34701f6bcda4a3f2ffff8f389322cd7f0788fbc7
NIkhil Kumar Singh <nks130501@gmail.com>
2024-01-22 19:52:20 +0530

    changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -252,7 +252,8 @@ export const project = [
     id: 4,
     title: "Blog Web App",
     date: "Jan 2024",
-    description: "A feature-rich Blogging Web App using Next.js, where users can seamlessly engage with content through a user-friendly interface. This project incorporates Google authentication for a secure and convenient login process, ensuring that only authenticated users can create posts and comment on them. The app is structured with multiple sections based on categories, providing a personalized experience for users to explore and contribute within their areas of interest. ",
+    description: 
+    "I developed a Next.js Blogging Web App with Google authentication, ensuring a secure and user-friendly experience. Users can create posts and comments in categorized sections, emphasizing my proficiency in Next.js and dynamic content management.",
     image: "https://raw.githubusercontent.com/Nikks-13/Just_Blog/main/screenshot/screenshot.png",
     tags: ["React Js","Next js","Firebase","Mongodb","Prisma","Toastify","Google Oauth"],
     category: "web app",
@@ -271,12 +272,12 @@ export const project = [
     id: 5,
     title: "Weather IOS App",
     date: "Jan 2024",
-    description: "A Simple weather app using React Native, incorporating real-time weather data from a reliable API. One of the key features of the app is the seamless integration of async storage, allowing users to effortlessly save and retrieve last searched location weather . The app provides an intuitive and user-friendly interface, ensuring a smooth and enjoyable experience for users seeking accurate and up-to-date weather information.",
+    description: "I Simple React Native weather app with real-time data, featuring seamless async storage for saving last searched locations. The user-friendly interface delivers accurate and up-to-date weather information, showcasing my skills in React Native development.",
     image: "https://raw.githubusercontent.com/Nikks-13/Weather-App/main/screenshort/home.png",
     tags: ["React-Native","Weather API","Async Storage","Expo"],
     category: "Mobile App",
     github: "https://github.com/Nikks-13/Weather-App",
-    webapp: "https://private-user-images.githubusercontent.com/90950696/296507511-0a1e46c2-d0ed-4b68-bbef-783e2c838ca4.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDU5Mjc1NDksIm5iZiI6MTcwNTkyNzI0OSwicGF0aCI6Ii85MDk1MDY5Ni8yOTY1MDc1MTEtMGExZTQ2YzItZDBlZC00YjY4LWJiZWYtNzgzZTJjODM4Y2E0Lm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTIyVDEyNDA0OVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ4NTc5YjFjMmI1NjUxMzhmMzVkMDgyNzE5Njc2MWVmZGI4ZTg2ZGM4MDYxMzdlZDA2ZTdkYWFjNWU5NmY0OTAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.hTRr18kgu1EivskGzz6OcS70K5kYK-UGWQpAJi6bcbs",
+    webapp: "https://github.com/Nikks-13/Weather-App/assets/90950696/0a1e46c2-d0ed-4b68-bbef-783e2c838ca4",
     member: [
       {
         name: "Nikhil Kumar singh",
@@ -290,13 +291,13 @@ export const project = [
     id: 6,
     title: "Just Snaxx Food IOS App",
     date: "Jan 2024",
-    description: "A dynamic food app using React Native, offering users a seamless experience in selecting and customizing multiple food and drink items to add to their carts. The app features a user-friendly interface that facilitates easy navigation and selection, allowing users to effortlessly manage their orders. In addition to the cart functionality, the app enables users to curate a personalized favorites list for quick access to preferred items. Post-checkout, users can conveniently track their order history, enhancing the overall user experience. This project showcases my adept skills in React Native, UI/UX design, and efficient data management, making it a compelling asset to highlight on my resume.",
+    description: "A dynamic React Native food app, providing users with a seamless ordering experience. The user-friendly interface allows easy customization and management of multiple items in the cart, along with the convenience of tracking order history. ",
 
     image: "https://raw.githubusercontent.com/Nikks-13/Just_Snaxx_App/main/screenshot/demo.png",
     tags: ["React-Native","Lottie-Files","Async Storage","React-Toast","TypeScript"],
     category: "Mobile App",
     github: "https://github.com/Nikks-13/Just_Snaxx_App",
-    webapp: "https://private-user-images.githubusercontent.com/90950696/298436109-c40ce6da-0467-473f-b816-d4179244e0be.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDU5Mjc3MDEsIm5iZiI6MTcwNTkyNzQwMSwicGF0aCI6Ii85MDk1MDY5Ni8yOTg0MzYxMDktYzQwY2U2ZGEtMDQ2Ny00NzNmLWI4MTYtZDQxNzkyNDRlMGJlLm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTIyVDEyNDMyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTA1NzQ3MjEzYjJjY2RjNTA5MGIwZTk2YTU1YTA2ZDY3Y2UwNGFkM2I5MTUwZWJhYTExOWUxZmQzZjQyNDRlZTcmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.w-IR1KpDuZCvOc8NfbYzWYX9nsEGE6wRcCRPh0N4mYk",
+    webapp: "",
     member: [
       {
         name: "Nikhil Kumar singh",
